d1b09ff2c80791f524b8e5efa806ff256567989d
atenagm <ashenatena@gmail.com>
2020-07-08 11:57:39 +0430

    Add implementations of Data class

--- utils/data.py
+++ utils/data.py
@@ -8,31 +8,64 @@ OBJECT RECOGNITION USING A SPIKING NEURAL NETWORK.
 
 """
 
+import os
+
+import numpy as np
+import pandas as pd
+from PIL import Image
+from sklearn.model_selection import train_test_split
+
 
 class Data:
-    # TODO add attributes, fill methods, edit docs.
-    """class Data."""
+    """Data class."""
 
-    def __init__(self):
-        pass
+    def __init__(self, path, classes, image_size=(100, 100)):
+        self.classes = classes
+        self.n_classes = len(classes)
+        self.data_frame = pd.DataFrame()
+        self.train_idx = []
+        self.test_idx = []
 
-    def split_train_test(self, test_ratio=0.3, shuffle=True,
-                         randome_state=None):
+        x = []
+        y = []
+        for obj in classes:
+            cls_path = path + ("/" if path[-1] != "/" else "") + obj + "/"
+            for img_path in os.listdir(cls_path):
+                img = Image.open(cls_path).resize(image_size)
+                x.append(np.asarray(img))
+                y.append(obj)
+
+        self.n_samples = len(self.y)
+
+        self.data_frame = pd.DataFrame({"x": x, "y": y}, columns=["x", "y"])
+
+        enc = pd.get_dummies(self.data_frame["y"])
+        self.data_frame = pd.concat([self.data_frame, enc], axis=1)
+
+    def split_train_test(self, test_ratio=0.3):
         """
-        Split train and test sets.
+        Split train and test samples.
 
         Parameters
         ----------
-        test_ratio : TYPE, optional
-            DESCRIPTION. The default is 0.3.
-        shuffle : TYPE, optional
-            DESCRIPTION. The default is True.
-        randome_state : TYPE, optional
-            DESCRIPTION. The default is None.
+        test_ratio : float, optional
+            The ratio of test samples. The default is 0.3.
 
         Returns
         -------
-        None.
+        x_train : pandas.Series
+            Train image data.
+        x_test : pandas.Series
+            Test image data.
+        y_train : pandas.DataFrame
+            Train class labels.
+        y_test : pandas.DataFrame
+            Test class labels.
 
         """
-        pass
+        x_train, x_test, y_train, y_test = train_test_split(
+                self.data_frame["x"], self.data_frame.iloc[:, -self.n_classes:],
+                test_ratio=test_ratio, shuffle=True)
+        self.train_idx = list(x_train.index)
+        self.test_idx = list(x_test.index)
+        return x_train, x_test, y_train, y_test
